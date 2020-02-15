5f4a230d55b95a5ad5cc82a53e9ac522fb44efe6
Azhar Uddin Azad <azhar.u.azad@gmail.com>
2020-02-15 23:36:09 +0600

    final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm, FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'demo-ng-app';
+  title = 'ng-contact-form';
+
+  contactForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { 
+    this.createContactForm();
+  }
+
+  onTemplateFormSubmit(form: NgForm) {
+    console.log('Your template-based form data: ', form.value);
+  }
+
+  onReactiveFormSubmit() {
+    console.log('Your reactive form data: ', this.contactForm.value);
+  }
+
+  createContactForm() {
+    this.contactForm = this.formBuilder.group({
+      fullName: [''],
+      email: [''],
+      message: ['']
+    });
+  }
 }
